d2b7270d6e9c8ab2cefcce31006a8201899a5253
sissis02 <sissis1993@gmail.com>
2023-10-25 14:55:26 +0200

    fetch API

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,40 +1,58 @@
-import Counter from "./components/Counter";
-import logo from "./assets/logo.svg";
-
-import "./App.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import DisplayHero from "./components/DisplayHero";
 
 function App() {
+  const [heros, setHeros] = useState([]);
+  const endpoints = [
+    "https://www.superheroapi.com/api.php/10232380959845753/346",
+    "https://www.superheroapi.com/api.php/10232380959845753/310",
+    "https://www.superheroapi.com/api.php/10232380959845753/450",
+    "https://www.superheroapi.com/api.php/10232380959845753/289",
+    "https://www.superheroapi.com/api.php/10232380959845753/405",
+    "https://www.superheroapi.com/api.php/10232380959845753/303",
+    "https://www.superheroapi.com/api.php/10232380959845753/70",
+    "https://www.superheroapi.com/api.php/10232380959845753/149",
+    "https://www.superheroapi.com/api.php/10232380959845753/574",
+    "https://www.superheroapi.com/api.php/10232380959845753/622",
+    "https://www.superheroapi.com/api.php/10232380959845753/644",
+    "https://www.superheroapi.com/api.php/10232380959845753/655",
+    "https://www.superheroapi.com/api.php/10232380959845753/176",
+    "https://www.superheroapi.com/api.php/10232380959845753/208",
+    "https://www.superheroapi.com/api.php/10232380959845753/423",
+    "https://www.superheroapi.com/api.php/10232380959845753/287",
+    "https://www.superheroapi.com/api.php/10232380959845753/352",
+    "https://www.superheroapi.com/api.php/10232380959845753/502",
+    "https://www.superheroapi.com/api.php/10232380959845753/717",
+    "https://www.superheroapi.com/api.php/10232380959845753/370",
+    "https://www.superheroapi.com/api.php/10232380959845753/729",
+    "https://www.superheroapi.com/api.php/10232380959845753/687",
+    "https://www.superheroapi.com/api.php/10232380959845753/526",
+    "https://www.superheroapi.com/api.php/10232380959845753/485",
+  ];
+
+  function fetchHeroes() {
+    axios
+      .all(endpoints.map((endpoint) => axios.get(endpoint)))
+      .then((res) => setHeros(res))
+      .catch((err) => console.info(err));
+  }
+
+  useEffect(() => {
+    fetchHeroes();
+  }, []);
+
+  console.info(heros);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React !</p>
-
-        <Counter />
-
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {" | "}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div>
+      {heros.map((hero) => (
+        <DisplayHero
+          key={hero.data.name}
+          url={hero.data.image.url}
+          name={hero.data.name}
+        />
+      ))}
     </div>
   );
 }
