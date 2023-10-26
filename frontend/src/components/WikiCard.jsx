ba62cc43d47876e530d91c09e0ca81f7e4ea9e18
sissis02 <sissis1993@gmail.com>
2023-10-26 16:06:56 +0200

    display stats of heros

---
+++ frontend/src/components/WikiCard.jsx
@@ -0,0 +1,118 @@
+import PropTypes from "prop-types";
+
+function WikiCard({
+  name,
+  fullname,
+  alias,
+  birth,
+  intelligence,
+  durability,
+  strength,
+  power,
+  speed,
+  combat,
+}) {
+  return (
+    <div className="wikiCard">
+      <div className="WikiCard-top">
+        <h6>{name}</h6>
+        <h5>Full name :</h5>
+        {fullname !== "-" ? <p>{fullname}</p> : <p>???</p>}
+        <h5>Alias :</h5>
+        {alias !== "-" ? <p>{alias}</p> : <p>???</p>}
+        <h5>Place of birth :</h5>
+        {birth !== "-" ? <p>{birth}</p> : <p>???</p>}
+      </div>
+      <div className="wikiCard-bottom">
+        <h6>Stats</h6>
+        <label htmlFor="intelligence">intelligence :</label>
+        <meter
+          id="intelligence"
+          min="0"
+          max="100"
+          low="33"
+          high="66"
+          optimum="80"
+          value={intelligence}
+        >
+          at 50/100
+        </meter>
+        <label htmlFor="durability">Durability :</label>
+        <meter
+          id="durability"
+          min="0"
+          max="100"
+          low="33"
+          high="66"
+          optimum="80"
+          value={durability}
+        >
+          at 50/100
+        </meter>
+        <label htmlFor="strength">Strength :</label>
+        <meter
+          id="dstrength"
+          min="0"
+          max="100"
+          low="33"
+          high="66"
+          optimum="80"
+          value={strength}
+        >
+          at 50/100
+        </meter>
+        <label htmlFor="power">Power :</label>
+        <meter
+          id="power"
+          min="0"
+          max="100"
+          low="33"
+          high="66"
+          optimum="80"
+          value={power}
+        >
+          at 50/100
+        </meter>
+        <label htmlFor="speed">Speed :</label>
+        <meter
+          id="speed"
+          min="0"
+          max="100"
+          low="33"
+          high="66"
+          optimum="80"
+          value={speed}
+        >
+          at 50/100
+        </meter>
+        <label htmlFor="combat">Combat :</label>
+        <meter
+          id="combat"
+          min="0"
+          max="100"
+          low="33"
+          high="66"
+          optimum="80"
+          value={combat}
+        >
+          at 50/100
+        </meter>
+      </div>
+    </div>
+  );
+}
+
+WikiCard.propTypes = {
+  name: PropTypes.string.isRequired,
+  fullname: PropTypes.string.isRequired,
+  alias: PropTypes.string.isRequired,
+  birth: PropTypes.string.isRequired,
+  intelligence: PropTypes.number.isRequired,
+  durability: PropTypes.number.isRequired,
+  strength: PropTypes.number.isRequired,
+  power: PropTypes.number.isRequired,
+  speed: PropTypes.number.isRequired,
+  combat: PropTypes.number.isRequired,
+};
+
+export default WikiCard;
