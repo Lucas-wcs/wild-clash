ff03a8a0a48654346ffb1308aeaa8ef45662dc97
Emmanuelle BONOLI <manu.bonoli@gmail.com>
2023-10-26 09:20:51 +0200

    maj page Fight + barre de vie

---
+++ frontend/src/components/HealthBar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function HealthBar() {
+  return (
+    <div className="healthBar">
+      <div className="healthBarFighters">healthBar1</div>
+      <div className="healthBarFighters">healthBar2</div>
+    </div>
+  );
+}
+
+export default HealthBar;
