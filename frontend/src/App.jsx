51823014cf998a947464b8fd9605acff0512ec58
Emmanuelle BONOLI <manu.bonoli@gmail.com>
2023-10-27 16:47:57 +0200

    obliger l'utilisateur au mode paysage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,17 +2,31 @@ import React from "react";
 import axios from "axios";
 import { Outlet } from "react-router-dom";
 import Header from "./components/Header";
+import useScreenOrientation from "./components/useScreenOrientation";
 
 function App() {
+  const orientation = useScreenOrientation();
+  // return <p>{orientation}</p>;
+
   return (
-    <div className="body">
-      <div className="header">
-        <Header />
-      </div>
-      <Outlet />
+    <div>
+      {orientation === "portrait-primary" ? (
+        <div className="orientationPortrait">
+          Pour une meilleure expérience avec le jeu CLASH HEROES, merci de
+          tourner votre appareil.
+        </div>
+      ) : (
+        <div className="body">
+          <div className="header">
+            <Header />
+          </div>
+          <Outlet />
+        </div>
+      )}
     </div>
   );
 }
+
 export default App;
 export const loadDatas = async () => {
   const endpoints = [
