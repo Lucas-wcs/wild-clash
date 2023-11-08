61ff3ba5f65fcf3683fdf0bca9fb386c1f58209f
Emmanuelle BONOLI <manu.bonoli@gmail.com>
2023-11-08 16:10:59 +0100

    responsive healthBar + move name from fightPage

--- frontend/src/components/HealthBar.jsx
+++ frontend/src/components/HealthBar.jsx
@@ -1,10 +1,23 @@
 import { motion, animate } from "framer-motion";
 import { PropTypes } from "prop-types";
-import { useEffect, useRef } from "react";
+import { useState, useEffect, useRef } from "react";
+import { useRouteLoaderData } from "react-router-dom";
 
-function HealthBar({ value, value2 }) {
+function HealthBar({ value, value2, imgFighter1, imgFighter2 }) {
   const progressTextRef = useRef(null);
   const progressTextRef2 = useRef(null);
+
+  const heroes = useRouteLoaderData("app");
+  const [heroName1, setHeroName1] = useState("");
+  const [heroName2, setHeroName2] = useState("");
+  useEffect(() => {
+    const hero1 = heroes.find((hero) => hero.data.image.url === imgFighter1);
+    const hero2 = heroes.find((hero) => hero.data.image.url === imgFighter2);
+    if (hero1 && hero2) {
+      setHeroName1(hero1.data.name);
+      setHeroName2(hero2.data.name);
+    }
+  }, [heroes, imgFighter1, imgFighter2]);
 
   useEffect(() => {
     if (progressTextRef.current != null) {
@@ -30,38 +43,50 @@ function HealthBar({ value, value2 }) {
 
   return (
     <div className="healthBar">
-      <div className="healthBarContainer">
-        <div className="levelhealthBar">
-          <motion.div
-            className="bar"
-            animate={{
-              width: `${value}%`,
-            }}
-            transition={{
-              duration: 2.5,
-            }}
-          />
+      <div className="namePlayers">
+        <div className="nameHero">{heroName1}</div>
+        <div className="nameEnemy">{heroName2}</div>
+      </div>
+
+      <div className="healthBar2players">
+        <div className="healthBar1Player">
+          <div className="healthBarContainer">
+            <div className="levelhealthBar">
+              <motion.div
+                className="bar"
+                animate={{
+                  width: `${value}%`,
+                }}
+                transition={{
+                  duration: 2.5,
+                }}
+              />
+            </div>
+          </div>
+          <div className="textLevelHealthBar">
+            <p ref={progressTextRef}>0</p>
+            <p>pv</p>
+          </div>
         </div>
-      </div>
-      <div className="textLevelHealthBar">
-        <p ref={progressTextRef}>0</p>
-        <p>pv</p>
-      </div>
-      <div className="textLevelHealthBar">
-        <p ref={progressTextRef2}>0</p>
-        <p>pv</p>
-      </div>
-      <div className="healthBarContainer">
-        <div className="levelhealthBar">
-          <motion.div
-            className="bar"
-            animate={{
-              width: `${value2}%`,
-            }}
-            transition={{
-              duration: 2.5,
-            }}
-          />
+
+        <div className="healthBar1Player">
+          <div className="textLevelHealthBar">
+            <p ref={progressTextRef2}>0</p>
+            <p>pv</p>
+          </div>
+          <div className="healthBarContainer">
+            <div className="levelhealthBar">
+              <motion.div
+                className="bar"
+                animate={{
+                  width: `${value2}%`,
+                }}
+                transition={{
+                  duration: 2.5,
+                }}
+              />
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -71,6 +96,8 @@ function HealthBar({ value, value2 }) {
 HealthBar.propTypes = {
   value: PropTypes.number.isRequired,
   value2: PropTypes.number.isRequired,
+  imgFighter1: PropTypes.string.isRequired,
+  imgFighter2: PropTypes.string.isRequired,
 };
 
 export default HealthBar;
