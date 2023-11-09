9f3921041ddd012b5377300affba6891d9885254
Techer Tony <techer307@gmail.com>
2023-11-09 17:19:46 +0100

    choose Hero ok

--- frontend/src/components/DisplayHero.jsx
+++ frontend/src/components/DisplayHero.jsx
@@ -4,19 +4,28 @@ function DisplayHero({
   url,
   name,
   selectHero1,
+  selectHero2,
   setSelectHero1,
   setSelectHero2,
 }) {
   function handleSelected() {
     if (selectHero1 === "") {
-      setSelectHero1(url);
-    } else {
+      if (selectHero2 !== url) {
+        setSelectHero1(url);
+      }
+    } else if (selectHero1 !== url) {
       setSelectHero2(url);
     }
   }
 
   return (
-    <button type="button" className="choiceHero" onClick={handleSelected}>
+    <button
+      type="button"
+      className={`choiceHero ${
+        selectHero1 === url || selectHero2 === url ? "greyPerso" : ""
+      }`}
+      onClick={handleSelected}
+    >
       <img key={url} className="sizeChooseFighter" src={url} alt={name} />
     </button>
   );
@@ -26,6 +35,7 @@ DisplayHero.propTypes = {
   url: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
   selectHero1: PropTypes.string.isRequired,
+  selectHero2: PropTypes.string.isRequired,
   setSelectHero1: PropTypes.string.isRequired,
   setSelectHero2: PropTypes.string.isRequired,
 };
