33a94ee1e7bfde867d4724c5e463bf529e3104f9
Titouan <fanch.gris@gmail.com>
2023-11-10 10:38:06 +0100

    Rideau + FX/SFX v1

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,26 +1,43 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import LaunchButton from "../components/LaunchButton";
 import WikiButton from "../components/WikiButton";
 
 function Home() {
   const [isOpenCurtain, setIsOpenCurtain] = useState(false);
+  const [isFlash, setIsFlash] = useState("");
+  const audio = useRef(null);
+  const button = useRef(null);
 
   function handleCurtain() {
     setIsOpenCurtain(!isOpenCurtain);
+    setTimeout(() => {
+      setIsFlash("flash");
+    }, 500);
+    setTimeout(() => {
+      audio.current.muted = false;
+      audio.current.play();
+    }, 1000);
   }
   return (
-    <div className="home">
+    <div className={`home ${isFlash}`}>
       <div className="curtain">
         <div
           className={`curtain-panel-left ${
             isOpenCurtain ? "openAnimationL" : "closeAnimationL"
           }`}
         >
-          <div className="button-wrapper">
-            <button type="button" onClick={handleCurtain} className="logo-1">
-              <img src="./public/images/logo_1-1.png" alt="" />
-            </button>
-          </div>
+          <button
+            type="button"
+            onClick={handleCurtain}
+            className="logo-1"
+            ref={button}
+          >
+            <img
+              className="logoButtonL"
+              src="./public/images/logo_1-1.png"
+              alt=""
+            />
+          </button>
         </div>
 
         <div
@@ -29,19 +46,28 @@ function Home() {
           }`}
         >
           <button type="button" onClick={handleCurtain} className="logo-2">
-            <img src="./public/images/logo_2.png" alt="" />
+            <audio className="storm" ref={audio} muted>
+              <track kind="captions" />
+              <source src="/sons/tonnerre.mp3" type="audio/mp3" />
+            </audio>
+            <img
+              className="logoButtonR"
+              src="./public/images/logo_2.png"
+              alt=""
+            />
           </button>
         </div>
       </div>
-      <img
-        className="homelogo"
-        src="public/images/homeLogo.png"
-        alt="homeLogo"
-      />
-      <img className="homelogo" src="/images/homeLogo.png" alt="homeLogo" />
-      <div className="homeButtons">
-        <LaunchButton />
-        <WikiButton className="wikiButton" />
+      <div className={`home2 ${isFlash}`}>
+        <img
+          className="homelogo"
+          src="public/images/homeLogo.png"
+          alt="homeLogo"
+        />
+        <div className="homeButtons">
+          <LaunchButton />
+          <WikiButton className="wikiButton" />
+        </div>
       </div>
     </div>
   );
