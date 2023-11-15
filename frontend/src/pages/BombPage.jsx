fd39c9f536dcb31bbb62f6f1310f62785ff4208d
Techer Tony <techer307@gmail.com>
2023-11-15 11:04:01 +0100

    design page Bomb

--- frontend/src/pages/BombPage.jsx
+++ frontend/src/pages/BombPage.jsx
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 
 function BombPage() {
-  return <div>Hello</div>;
-}
+  const [timer, setTimer] = useState(60);
 
+  const handleDecrementTime = () => {
+    const myInterval = setInterval(() => {
+      setTimer((oldValue) => {
+        if (oldValue === 1) {
+          clearInterval(myInterval);
+        }
+        return oldValue - 1;
+      });
+    }, 700);
+  };
+
+  return (
+    <div className="BombPage">
+      <h1 className="titlePageBomb">Defuse The Bomb</h1>
+      <div className="ContainerImageBomb">
+        <p>{timer}</p>
+      </div>
+      <div className="largeBox">
+        <div className="concurentContainer">
+          <div className="inlineDiv" />
+        </div>
+        <button
+          type="button"
+          className="ButtonBombFight"
+          onClick={handleDecrementTime}
+        >
+          Start Counting
+        </button>
+        <div className="secondConcurentContainer">
+          <div className="inlineDiv" />
+        </div>
+      </div>
+    </div>
+  );
+}
 export default BombPage;
