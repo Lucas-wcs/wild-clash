fba4569af86c4fa3d887cf65a731bdc7de44db8e
Titouan Gris <fanch.gris@gmail.com>
2023-11-17 10:35:42 +0100

    suppression de public dans les paths et rajouts de sons

--- frontend/src/pages/FightPage.jsx
+++ frontend/src/pages/FightPage.jsx
@@ -6,12 +6,19 @@ import HealthBar from "../components/HealthBar";
 function FightPage() {
   const { selectHero1, selectHero2 } = useContext(HeroLoaderContext);
   const audio = useRef(null);
+  const audio2 = useRef(null);
 
   useEffect(() => {
     if (audio.current) {
       audio.current.muted = false;
       audio.current.play();
     }
+    setTimeout(() => {
+      if (audio2.current) {
+        audio2.current.muted = false;
+        audio2.current.play();
+      }
+    }, 3800);
   }, [audio]);
 
   return (
@@ -20,6 +27,13 @@ function FightPage() {
         <track kind="captions" />
         <source
           src="/sons/three-two-one-fight-deep-voice-38382.mp3"
+          type="audio/mp3"
+        />
+      </audio>
+      <audio ref={audio2} muted>
+        <track kind="captions" />
+        <source
+          src="/sons/techno-syndrome-mortal-kombat-song-by-the-immortals_NaxR0wNS.mp3"
           type="audio/mp3"
         />
       </audio>
